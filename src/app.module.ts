5e3f2bcbb6cdef9c2d68f7cbefbc0cc9773ad541
李* <jiandinglixiang@qq.com>
2024-12-24 15:07:48 +0800

    refactor: first init
    - 删除了与 MongoDB 相关的模块、服务和配置
    - 更新了 Docker配置文件，移除了 MongoDB相关部分
    - 删除了 MongoDB 种子数据相关文件
    - 更新了用户、角色、状态等实体的数据库相关代码
    -移除了 Hygen 模板中与 MongoDB 相关的部分

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,41 +8,21 @@ import appConfig from './config/app.config';
 import mailConfig from './mail/config/mail.config';
 import fileConfig from './files/config/file.config';
 import facebookConfig from './auth-facebook/config/facebook.config';
-import googleConfig from './auth-google/config/google.config';
-import twitterConfig from './auth-twitter/config/twitter.config';
-import appleConfig from './auth-apple/config/apple.config';
 import path from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthAppleModule } from './auth-apple/auth-apple.module';
 import { AuthFacebookModule } from './auth-facebook/auth-facebook.module';
-import { AuthGoogleModule } from './auth-google/auth-google.module';
-import { AuthTwitterModule } from './auth-twitter/auth-twitter.module';
 import { HeaderResolver, I18nModule } from 'nestjs-i18n';
-import { TypeOrmConfigService } from './database/typeorm-config.service';
 import { MailModule } from './mail/mail.module';
 import { HomeModule } from './home/home.module';
-import { DataSource, DataSourceOptions } from 'typeorm';
 import { AllConfigType } from './config/config.type';
 import { SessionModule } from './session/session.module';
 import { MailerModule } from './mailer/mailer.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './database/mongoose-config.service';
-import { DatabaseConfig } from './database/config/database-config.type';
 
-// <database-block>
-const infrastructureDatabaseModule = (databaseConfig() as DatabaseConfig)
-  .isDocumentDatabase
-  ? MongooseModule.forRootAsync({
-      useClass: MongooseConfigService,
-    })
-  : TypeOrmModule.forRootAsync({
-      useClass: TypeOrmConfigService,
-      dataSourceFactory: async (options: DataSourceOptions) => {
-        return new DataSource(options).initialize();
-      },
-    });
-// </database-block>
+const infrastructureDatabaseModule = MongooseModule.forRootAsync({
+  useClass: MongooseConfigService,
+});
 
 @Module({
   imports: [
@@ -55,9 +35,6 @@ const infrastructureDatabaseModule = (databaseConfig() as DatabaseConfig)
         mailConfig,
         fileConfig,
         facebookConfig,
-        googleConfig,
-        twitterConfig,
-        appleConfig,
       ],
       envFilePath: ['.env'],
     }),
@@ -89,9 +66,6 @@ const infrastructureDatabaseModule = (databaseConfig() as DatabaseConfig)
     FilesModule,
     AuthModule,
     AuthFacebookModule,
-    AuthGoogleModule,
-    AuthTwitterModule,
-    AuthAppleModule,
     SessionModule,
     MailModule,
     MailerModule,
